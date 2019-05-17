5e92f460fa7c241dfd4d09ed2611cc45ae8b33f3
judoc-43 <50613371+judoc-43@users.noreply.github.com>
2019-05-17 13:20:21 +0530

    Add files via upload

---
+++ spi com/ATmega16_SPI_Master/ATmega16_SPI_Master/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+ATmega16_SPI_Master.c
+
+LCD_16x2_C_file.c
+
+SPI_Master_C_file.c
+
